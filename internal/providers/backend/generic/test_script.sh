e2baefa4a8df4049c33de665171eaed868326273
Bartek Kus <7887446+bartekus@users.noreply.github.com>
2025-12-01 02:47:40 -0700

    Fix failing license-check

--- internal/providers/backend/generic/test_script.sh
+++ internal/providers/backend/generic/test_script.sh
@@ -1,2 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# Stagecraft - Stagecraft is a Go-based CLI that orchestrates local-first development and scalable single-host to multi-host deployments for multi-service applications powered by Docker Compose.
+#
+# Copyright (C) 2025  Bartek Kus
+#
+# This program is free software licensed under the terms of the GNU AGPL v3 or later.
+#
+# See https://www.gnu.org/licenses/ for license details.
 pwd
